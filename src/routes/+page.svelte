8bda0f055d886e4f8b3a6b8e8a221f4b685dc02c
Xd_Golden_Tiger <68713969+XdGoldenTigerOfficial@users.noreply.github.com>
2023-06-28 14:56:52 +1200

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+    <title>Home • Blueline Entertainment</title>
+</svelte:head>
+
+<div class="relative main-information hero h-full bg-base-200 justify-items-center">
+    <div class="hero-content text-center">
+        <div class="max-w-7xl">
+            <h1 class="py-6 text-5xl font-bold">Welcome to Blueline Entertainment</h1>
+            <p class="py-6 leading-8 text-lg"><b>Blueline Entertainment</b> is a <b>17+</b> serious role-play FiveM community.<br>We started at the beginning of 2022 with <b>Critical</b> being the Director. But at the beginning of August (2022), ownership of Blueline Entertainment was handed over to <b>Xd_Golden_Tiger</b> to keep the community running successfully and operations running smooth.</p>
+            <p class="py-6 leading-8 text-lg">Over the last couple of months, Blueline Entertainment has had a lot of updates to the servers and websites.<br>Please join our Discord server to stay up to date with future updates!</p><br>
+            <h1 class="py-3 text-3xl font-bold">Requirements:</h1>
+            <p class="leading-8 text-lg">- You must be <b>17</b> years of age on the day you apply. (No Exceptions).</p>
+            <p class="leading-8 text-lg">- You must have a working computer that you can use for in-game (Exluding Communications).</p>
+            <p class="leading-8 text-lg">- You must have a legal version of Grand Theft Auto V (PC) - (Excluding Communications).</p>
+            <p class="leading-8 text-lg">- You must <b>fully</b> understand and speak English. (No Exceptions).</p>
+            <p class="leading-8 text-lg">- You must have a working and clear sounding microphone/headset. (No background noises).</p>
+            <p class="leading-8 text-lg">- You must have <b>push to talk</b> enabled in-game (RP) <b>and</b> in the teamspeak server (RTO).</p>
+            <p class="leading-8 text-lg">- You must show respect to all members and staff at all time.</p>
+        </div>
+    </div>
+</div>
